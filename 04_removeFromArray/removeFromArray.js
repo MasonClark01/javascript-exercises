f6f47672cad0a29e8d7b6d0d8d2650375ed57d34
Mason Clark <109944632+MasonClark01@users.noreply.github.com>
2023-09-06 13:19:30 -0700

    Completed exercises 4 & 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let newArr = arr
+    for(const arg of args){
+        newArr = newArr.filter((ele)=> ele !== arg);
+    }
+    return newArr
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sumNums = 0
+    if(typeof num1 === 'number' && typeof num2 === 'number' && num1 >= 0 && num2 >= 0){
+        let min = 0
+        let out = 0
+        if(num1 > num2){
+            min = num2
+            for(let i = min; i <= num1; i++){
+                out += i
+            }
+        }
+        if(num2 > num1){
+            min = num1
+            for(let i = min; i <= num2; i++){
+                out += i
+            }
+        }
+        return out
+    }
+    else{
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
