cbe5130ad66a89efd6d33f13b6d2b3cec7ce622c
Mason Clark <109944632+MasonClark01@users.noreply.github.com>
2023-09-07 09:45:41 -0700

    Completed exercises 6&7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(Number.isInteger(year/4) === true){
+        if(Number.isInteger(year/100) === true){
+            if(Number.isInteger(year/400) === false){
+                return false
+            }
+            else if(Number.isInteger(year/400) === true){
+                return true
+            }
+        }
+        else if(Number.isInteger(year/100) === false){
+            return true
+        }
+    }
+    else{
+        return false
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let convCels = (fahr-32)*(5/9);
+  return Number(convCels.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let convFahr = cels*(9/5)+32;
+  return Number(convFahr.toFixed(1))
 };
 
 // Do not edit below this line
