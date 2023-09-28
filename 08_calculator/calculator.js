59a661f2d3395a4aabe485240896e615f1767caf
Mason Clark <109944632+MasonClark01@users.noreply.github.com>
2023-09-28 11:18:43 -0700

    Exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return(a + b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return(a - b)
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return(array.reduce((tot, plus) => tot + plus, 0))
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return(array.reduce((tot, mult) => tot * mult))
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0){
+    return 1
+  }
+  else{
+    let out = 1
+    for(let i = num; i > 0; i--){
+      out *= i
+    }
+    return out
+  }
 };
 
 // Do not edit below this line
