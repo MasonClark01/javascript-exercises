91c656cdaa62c7ab07b9a9a07e7b2187d1fb9d8e
Mason Clark <109944632+MasonClark01@users.noreply.github.com>
2023-09-29 09:36:21 -0700

    Exercise 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    let changedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "")
+    return(changedStr.split("").reverse().join("") === changedStr)
+}
 // Do not edit below this line
 module.exports = palindromes;
