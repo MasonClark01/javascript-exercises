af8242a53939466be409b5dea4fbb3ca27c25a29
Mason Clark <109944632+MasonClark01@users.noreply.github.com>
2023-10-01 15:53:18 -0700

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS"
+    }
+    let out = 1
+    let next = 0
+    for(let i = 2; i <= num; i++){
+        let curNum = out + next
+        next = out
+        out = curNum   
+    }
+    return out
 };
 
 // Do not edit below this line
